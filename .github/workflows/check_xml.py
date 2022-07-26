5158bd91631ffa72e1f4ae136e899f4b0522f6ee
Alex Chan <alex@alexwlchan.net>
2022-07-26 10:42:18 +0100

    Add checks for multiple <persName> nodes but no type="original"

--- .github/workflows/check_xml.py
+++ .github/workflows/check_xml.py
@@ -30,7 +30,7 @@ def get_file_paths_under(root=".", *, suffix=""):
 
 
 if __name__ == "__main__":
-    root = (
+    repo_root = (
         subprocess.check_output(
             [
                 "git",
@@ -44,20 +44,42 @@ if __name__ == "__main__":
 
     errors = 0
 
-    for path in sorted(get_file_paths_under(root, suffix=".xml")):
+    for path in sorted(get_file_paths_under(repo_root, suffix=".xml")):
 
-        # We don't parse the contents of the Templates directory as XML;
-        # it's meant for humans to read, not computers
-        if os.path.relpath(path, start=root).startswith("Templates/"):
+        # Exclude a couple of paths that aren't actual TEI files
+        if os.path.relpath(path, start=repo_root).startswith(("Templates/", "docs/")):
             continue
 
         try:
-            ET.parse(path)
+            root = ET.parse(path).getroot()
         except ET.ParseError as err:
             print("")
-            print(os.path.relpath(path, start=root))
-            print(f"\t{err}")
+            print(os.path.relpath(path, start=repo_root))
+            print(f"\tUnable to parse XML: {err}")
             errors += 1
+            continue
+
+        # The transformer expects to see author names in the form:
+        #
+        #       <author key="person_88345215">
+        #           <persName xml:lang="eng" type="standard">Badr-addin Muhammad b. Bahram b. Muhammad al-Qalanisi as-Samarqandi</persName>
+        #           <persName xml:lang="ar" type="original">بدر الدين محمد بن بهرام بن محمد القلانسي السمرقندي</persName>
+        #       </author>
+        #
+        # Notice that one is labelled `type="original"`; if this is missing,
+        # we don't know what contributor to display on the page.
+        for author in root.findall('.//{http://www.tei-c.org/ns/1.0}author'):
+            persname_nodes = author.findall('./{http://www.tei-c.org/ns/1.0}persName')
+
+            if len(persname_nodes) <= 1:
+                continue
+
+            if not any(pn.attrib.get('type') == 'original' for pn in persname_nodes):
+                print("")
+                print(os.path.relpath(path, start=repo_root))
+                print(f"\tFound <author> with multiple <persName> nodes but no type=\"original\"")
+                errors += 1
+                break
 
     print("")
 
