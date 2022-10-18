5e0e26f0cc7e9b4f4b24286781ba14b3f4f752dd
Alex Chan <alex@alexwlchan.net>
2022-10-18 16:49:22 +0100

    Move some of the XML checker into a common file

--- .github/workflows/check_xml.py
+++ .github/workflows/check_xml.py
@@ -12,22 +12,7 @@ import subprocess
 import sys
 from xml.etree import ElementTree as ET
 
-
-RED   = "\033[1;31m"
-GREEN = "\033[0;32m"
-RESET = "\033[0;0m"
-BLUE  = "\033[1;34m"
-
-
-def get_file_paths_under(root=".", *, suffix=""):
-    """Generates the paths to every file under ``root``."""
-    if not os.path.isdir(root):
-        raise ValueError(f"Cannot find files under non-existent directory: {root!r}")
-
-    for dirpath, _, filenames in os.walk(root):
-        for f in filenames:
-            if os.path.isfile(os.path.join(dirpath, f)) and f.lower().endswith(suffix):
-                yield os.path.join(dirpath, f)
+from tooling import RED, GREEN, RESET, BLUE, get_file_paths_under
 
 
 def guess_line_label(path, text):
